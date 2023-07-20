74a5b058c3f00199c005303a7832015716ee5b27
François G. Dorais <fgdorais@gmail.com>
2023-07-20 00:40:11 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Logic Library for Lean4
---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «logic4»
+
+@[default_target]
+lean_lib Logic
+
+require Std from git "https://github.com/leanprover/std4" @ "main"

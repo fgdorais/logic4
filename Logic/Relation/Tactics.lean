2c68487e492c86b75d7a2e7cae2587ec0a48b900
François G. Dorais <fgdorais@gmail.com>
2023-08-03 16:33:07 -0400

    feat: add Logic.Relation

---
+++ Logic/Relation/Tactics.lean
@@ -0,0 +1,28 @@
+import Logic.Relation.Classes
+
+syntax "reflexivity" ("using" term:max)? : tactic
+macro_rules
+| `(tactic|reflexivity) => `(tactic|apply  Logic.Reflexive.rfl)
+| `(tactic|reflexivity using $r:term) => `(tactic|apply  Logic.Reflexive.rfl (r:=$r))
+
+syntax "irreflexivity" ("using" term:max)? : tactic
+macro_rules
+| `(tactic|irreflexivity) => `(tactic|apply  Logic.Irreflexive.irrfl)
+| `(tactic|irreflexivity using $r:term) => `(tactic|apply  Logic.Irreflexive.irrfl (r:=$r))
+
+syntax "symmetry" ("using" term:max ("," term:max)?)? : tactic
+macro_rules
+| `(tactic|symmetry) => `(tactic|first|apply  Logic.HSymmetric.symm _)
+| `(tactic|symmetry using $r) => `(tactic|apply  Logic.Symmetric.symm (r:=$r) _)
+| `(tactic|symmetry using $r, $s) => `(tactic|apply  Logic.HSymmetric.symm (r:=$r) (s:=$s) _)
+
+syntax "antisymmetry" "using" term:max ("," term:max)? : tactic
+macro_rules
+| `(tactic|antisymmetry using $r) => `(tactic|apply  Logic.Antisymmetric.antisymm (r:=$r))
+| `(tactic|antisymmetry using $r, $s) => `(tactic|apply  Logic.HAntisymmetric.antisymm (r:=$r) (s:=$s))
+
+syntax "transitivity" term:max ("using" term:max ("," term:max)?)? : tactic
+macro_rules
+| `(tactic|transitivity $y) => `(tactic|apply  Logic.HTransitive.trans (y:=$y))
+| `(tactic|transitivity $y using $r) => `(tactic|apply  Logic.Transitive.trans (r:=$r) (y:=$y))
+| `(tactic|transitivity $y using $r, $s) => `(tactic|apply  Logic.HTransitive.trans (r:=$r) (s:=$s) (y:=$y))

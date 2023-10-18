9289e8cf063c54ea66effd2d0436865b71454f8e
F. G. Dorais <fgdorais@gmail.com>
2023-10-17 22:56:14 -0400

    chore: update lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,9 @@
 import Lake
 open Lake DSL
 
-package «logic4»
+package Logic
 
 @[default_target]
 lean_lib Logic
 
-require Std from git "https://github.com/fgdorais/std4" @ "nat-all"
+require std from git "https://github.com/fgdorais/std4" @ "nat-all"
